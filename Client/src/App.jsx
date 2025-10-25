564911ac228a96a9b8408c1c29861cbf2463b580
coDing-Knight99 <dhruvkanojiawork@gmail.com>
2025-10-25 16:36:38 +0530

    Two

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import Login from './pages/login'
+import Login from './pages/Login.jsx'
 import Navbar from './components/Navbar'
 import  SnippetFilter from './components/Search'
 import Home from './pages/Home'
